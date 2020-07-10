f3a14842484614d832cf687ba82bf73e801299f4
s-omas <49887549+s-omas@users.noreply.github.com>
2020-07-10 10:44:54 -0600

    add validation and file writing

--- django_forms/myapp/load_json.py
+++ django_forms/myapp/load_json.py
@@ -1,5 +1,7 @@
 import json
 
+#adds all bottom-level key-value pairs in config file to python dict
+
 def denest():
     with open('/Users/simonthomas/music-box-io1/django_forms/myapp/static/my_config.json') as f:
         data = json.loads(f.read())
@@ -23,27 +25,29 @@ def denest():
     return new_dict
 
 
-#
-# denest()
-#
-# config = denest()
-# with open('/Users/simonthomas/music-box-io1/django_forms/myapp/static/schema/schema.json') as f:
-#     data = json.loads(f.read())
-#
-# properties = data['properties']
-#
-# for x in properties:
-#     section = properties[x]
-#     subsection = section["properties"]
-#     for y in subsection:
-#         print(subsection[y])
-#         print[config[y]['title']]
-#
-#     # if subsection[y]['additionalProperties']:
-#            # data['properties'][x][y]['properties']['default'] = config[y]
-#        # else:
-#            # data['properties'][x][y]['default'] = config[y]
-#
-#
-# with open('/Users/simonthomas/music-box-io1/django_forms/myapp/static/schema/populated_schema.json', 'w') as outfile:
-#     json.dump(data, outfile, indent=4)
+#populates config file with data from web form fields
+
+def repopulate():
+    with open('/Users/simonthomas/music-box-io1/django_forms/myapp/static/config_dict.json') as f:
+        config_dict = json.loads(f.read())
+
+    with open('/Users/simonthomas/music-box-io1/django_forms/myapp/static/my_config.json') as h:
+        config = json.loads(h.read())
+
+    for key in config:
+        nest1 = config[key]
+        for x in nest1:
+            if type(nest1[x]) is dict:
+                nest2 = nest1[x]
+                for y in nest2:
+                    config[key][x].update({y: config_dict[y]})
+            else:
+                config[key].update({x: config_dict[x]})
+
+    with open('/Users/simonthomas/music-box-io1/django_forms/myapp/static/my_config.json', 'w') as outfile:
+        json.dump(config, outfile, indent=4)
+
+
+
+
+
--- django_forms/myapp/views.py
+++ django_forms/myapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import CustomForm
+from json import dumps
+from .load_json import repopulate
+import json
 
 # Create your views here.
 
@@ -9,4 +12,18 @@ from .forms import CustomForm
 
 
 def some_view(request):
+
+    if request.method == 'POST':
+        form = CustomForm(request.POST)
+        if form.is_valid():
+            for key in form.cleaned_data:
+                print(key, form.cleaned_data[key])
+
+            with open('/Users/simonthomas/music-box-io1/django_forms/myapp/static/config_dict.json',
+                      'w') as outfile:
+                json.dump(form.cleaned_data, outfile, indent=4)
+
+            repopulate()
+
     return render(request, 'form.html', {'form': CustomForm()})
+
